b4c7900108b2807673a5cad94ed812392f1052fa
Arnab-dot <technofries1209@gmail.com>
2025-09-02 19:08:39 +0530

    Initial push: Ocean Landing Page and ripple effect

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import OceanLandingPage from './components/LandingPage'
+import './App.css'
+
+function App() {
+  return(
+    <div>
+      <OceanLandingPage />
+    </div>
+  )
+}
+ 
+
+export default App
---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './styles.css';
+
+const OceanLandingPage = () => {
+  return (
+    <div className="ocean-container">
+      {/* Navigation pane to be put here*/}
+      <nav className="navbar">
+        <div className="nav-content">
+          <h1 className="logo">Deep Sea</h1>
+        </div>
+      </nav>
+
+      {/* Here i have implemented bubbling */}
+      <div className="bubbles">
+        {[...Array(20)].map((_, i) => (
+          <span
+            key={i}
+            style={{
+              left: `${Math.random() * 100}%`,
+              animationDuration: `${4 + Math.random() * 6}s`,
+              animationDelay: `${Math.random() * 4}s`
+            }}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default OceanLandingPage;
